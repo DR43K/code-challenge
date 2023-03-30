707fc12c5cae6cd22a7d84668328ea5d48db222a
Juan Hernandez <juanr.hdz.gtz@gmail.com>
2023-03-29 23:16:28 -0600

    added string parser funtionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'code-challenge';
+  resultLabel: string = ''
+  encodedString: string = ''
+
+  onClickResult() {
+    var word = ["", "", ""]
+    var cont = 0
+
+    if (this.encodedString.length < 1 || this.encodedString.endsWith("0")) {
+      alert('Please insert a valid string');
+    } else {
+      const text = [...this.encodedString];
+
+      text.forEach(letter => {
+        if (letter != "0") {
+          word[cont] += letter
+        }
+        else if (word[cont].length != 0) {
+          cont += 1
+        }
+      });
+
+      this.resultLabel = `{"first_name": "${word[0]}", "last_name": "${word[1]}", "id": "${word[2]}"}`
+    }
+  }
 }
